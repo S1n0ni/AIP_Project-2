cf5b3fae1080ea4dc326c5d45cf574e3a6b8c453
S1n0ni <backvlad@mail.ru>
2025-01-19 16:08:16 +0300

    tc_9_4

--- app.js
+++ app.js
@@ -36,6 +36,11 @@ saveUninitialized: true,
 store: MongoStore.create({mongoUrl:'mongodb://localhost/tc2024'})
 }))
 
+app.use(function(req,res,next){
+  req.session.counter = req.session.counter + 1 || 1
+  next()
+  })  
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/books', books);
