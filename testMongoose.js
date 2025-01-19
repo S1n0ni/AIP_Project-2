0f1226058c92ae50ef6fbb2e6992eb5501712f12
S1n0ni <backvlad@mail.ru>
2025-01-19 15:20:42 +0300

    tc_7_3

--- testMongoose.js
+++ testMongoose.js
@@ -1,13 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-
-var schema = mongoose.Schema({ name: String })
-
-schema.methods.perc = function() {
-    console.log(this.name + " Перси Джексон")
-}
-
-const Book = mongoose.model('Book', schema);
-
-const Percy = new Book({ name: 'Повелитель молний' });
-Percy.save().then(() => Percy.perc());
+var Book = require('./models/book.js').Cat
+var book = new Book({
+title: "Похититель молний",
+nick: "TheLightningThief",
+})
+book.save();
