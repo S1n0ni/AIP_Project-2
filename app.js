a60f89744d60a00d658c2488af2000c8c36f1eb9
S1n0ni <backvlad@mail.ru>
2025-01-19 15:52:04 +0300

    tc_9_1

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/tc2024')
 
+var session = require("express-session")
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var books = require('./routes/books');
@@ -23,6 +25,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: "ThreeBooks",
+  cookie:{maxAge:60*1000},
+  proxy: true,
+  resave: true,
+  saveUninitialized: true
+  }))  
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
