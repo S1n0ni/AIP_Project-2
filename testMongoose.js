bee8301d9d7b0fb0adb60acfe68f8f6dc8a073e9
S1n0ni <96681942+S1n0ni@users.noreply.github.com>
2025-01-20 07:27:34 +0300

    tc_7_3

--- testMongoose.js
+++ testMongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-var Book = require('./models/book.js').Cat
+var Book = require('./models/book.js').Book
 var book = new Book({
 title: "Похититель молний",
 nick: "TheLightningThief",
